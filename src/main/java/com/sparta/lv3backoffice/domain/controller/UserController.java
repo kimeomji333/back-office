e2029fe766213ce9ef643ed01fecf34fb1196df2
Peter-Yu-0402 <peter.yu.0402@gmail.com>
2024-03-02 10:46:33 +0900

    [FIX] 리다이렉션 문제 해결
    UserController의 리다이렉션 문제 해결

--- src/main/java/com/sparta/lv3backoffice/domain/controller/UserController.java
+++ src/main/java/com/sparta/lv3backoffice/domain/controller/UserController.java
@@ -1,55 +1,83 @@
 package com.sparta.lv3backoffice.domain.controller;
 
+import com.sparta.lv3backoffice.domain.dto.user.LoginRequestDto;
 import com.sparta.lv3backoffice.domain.dto.user.SignupRequestDto;
+import com.sparta.lv3backoffice.domain.dto.user.SignupResponseDto;
+import com.sparta.lv3backoffice.domain.entity.User;
+import com.sparta.lv3backoffice.domain.repository.UserRepository;
 import com.sparta.lv3backoffice.domain.service.UserService;
+import com.sparta.lv3backoffice.global.exception.NotFoundException;
+import com.sparta.lv3backoffice.global.exception.UnauthorizedException;
 import jakarta.validation.Valid;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.function.Supplier;
 
 // 로그인, 가입 컨트롤러
 
 @Slf4j
-@Controller
+@RestController
 @RequestMapping("/api")
 public class UserController {
 
     private final UserService userService;
-
     public UserController(UserService userService) {
         this.userService = userService;
     }
 
-    @GetMapping("/user/login-page")
-    public String loginPage() {
-        return "login";
-    }
+//    @GetMapping("/user/login-page")
+//    public String loginPage() {
+//        return "login";
+//    }
+//
+//    @GetMapping("/user/signup")
+//    public String signupPage() {
+//        return "signup";
+//    }
 
-    @GetMapping("/user/signup")
-    public String signupPage() {
-        return "signup";
-    }
-
+    // 회원 가입
     @PostMapping("/user/signup")
-    public String signup(@Valid SignupRequestDto requestDto, BindingResult bindingResult) {
+    public ResponseEntity<?> signup(@Valid @RequestBody SignupRequestDto requestDto, BindingResult bindingResult) {
         // Validation 예외처리
         List<FieldError> fieldErrors = bindingResult.getFieldErrors();
-        if(fieldErrors.size() > 0) {
+        if(!fieldErrors.isEmpty()) {
             for (FieldError fieldError : bindingResult.getFieldErrors()) {
                 log.error(fieldError.getField() + " 필드 : " + fieldError.getDefaultMessage());
             }
-            return "redirect:/api/user/signup";
         }
 
-        userService.signup(requestDto);
+        return handleRequest(() -> {
+            userService.signup(requestDto);
+            return ResponseEntity.ok("성공적으로 회원가입이 완료되었습니다.");
+        });
+    }
 
-        return "redirect:/api/user/login-page";
+    // 로그인
+//    @PostMapping("/user/login")
+//    public ResponseEntity<?> login(@ResponseBody LoginRequestDto loginRequestDto) {
+//        // 이메일 기반으로 사용자 조회
+//
+//
+//    }
+
+
+    private ResponseEntity<?> handleRequest(Supplier<ResponseEntity<?>> supplier) {
+        try {
+            return supplier.get();
+        } catch (UnauthorizedException e) {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(e.getMessage());
+        } catch (NotFoundException e) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("인터넷 서버 오류: " + e.getMessage());
+        }
     }
 }
 
